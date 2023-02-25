65e2d6a7f78e01e433f317b0b54617a63e6edab7
adingapkgg <adingapkgg@outlook.com>
2023-02-26 03:45:29 +0800

    .

--- js/Visits.js
+++ js/Visits.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?e6ecc3329ffb366aa8a7654f53143ead";
+  hm.src = "https://hm.baidu.com/hm.js?078216bf8d5473dc3d5aec43f570ca97";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
