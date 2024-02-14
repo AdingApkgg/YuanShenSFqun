8c68c7432e6e2c2d8a27b338c01448ba285c92a1
AdingApkgg <AdingApkgg@outlook.com>
2024-02-15 03:38:18 +0800

    .

---
+++ uzz3phhz/js/main.js
@@ -0,0 +1,12 @@
+var searchTest = document.getElementById("searchTest");
+var name = prompt("请输入昵称！");
+document.getElementById("searchTest").value = name + "，请输入你想要的图片...";
+
+function searchTestClear() {
+	document.getElementById("searchTest").value = "";
+}
+
+function getCookie(name) {
+	var match = document.cookie.match(new RegExp('(^|)' + name + '=([^;]*)'));
+	if (match) return unescape(match[2]);
+}
